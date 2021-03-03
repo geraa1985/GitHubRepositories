4dbe90cf65857ea0110114a6210aaad50ad2007b
geraa1985 <geraa1985@gmail.com>
2021-03-03 17:37:57 +0300

    Initial commit

---
+++ app/src/main/java/com/geraa1985/githubrepositories/frameworks/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.geraa1985.githubrepositories.frameworks.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.geraa1985.githubrepositories.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
